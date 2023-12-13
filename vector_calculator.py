dc4ca358e7357e79765c9a9837d70d0643d7ac4e
Kimiya <kimiyazargari13@gmail.com>
2023-12-13 21:00:37 +0330

    add function to check if two vectors are orthogonal

--- vector_calculator.py
+++ vector_calculator.py
@@ -1,12 +1,14 @@
+#adds vector1 and vector2 and returns the result
 def addVectors(vector1, vector2):
     if len(vector1) != len(vector2):
         raise IndexError('Vectors must have the same numbers of coordinates')   
     else:
         result = []
         for coordinatesTuple in zip(vector1, vector2):
-            result.append(sum(coordinentsTuple))
+            result.append(sum(coordinatesTuple))
         return result
-    
+
+#adds vector2 from vector1 and returns the result   
 def subtractVectors(vector1, vector2):
     if len(vector1) != len(vector2):
         raise IndexError('Vectors must have the same numbers of coordinates')   
@@ -16,6 +18,7 @@ def subtractVectors(vector1, vector2):
             result.append(coordinatesTuple[0] - coordinatesTuple[1])
         return result
 
+#returns the dot product of vector1 and vector2
 def dotProduct(vector1, vector2):
      if len(vector1) != len(vector2):
         raise IndexError('Vectors must have the same numbers of coordinates')   
@@ -24,3 +27,10 @@ def dotProduct(vector1, vector2):
         for coordinatesTuple in zip(vector1, vector2):
             result += coordinatesTuple[0] * coordinatesTuple[1]
         return result
+#returns whether the two input vectors are orthogonal 
+def areOrthogonal(vector1, vector2):
+    if dotProduct(vector1, vector2) == 0:
+        return True
+    return False
+
+        
