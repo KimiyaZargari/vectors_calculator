da8f05e9bf8c227f6bbef197d878ddddc5fa16c7
Kimiya <kimiyazargari13@gmail.com>
2023-12-13 20:46:48 +0330

    add vector subtraction

--- vector_calculator.py
+++ vector_calculator.py
@@ -6,4 +6,12 @@ def addVectors(vector1, vector2):
         for coordinatesTuple in zip(vector1, vector2):
             result.append(sum(coordinentsTuple))
         return result
-
+    
+def subtractVectors(vector1, vector2):
+    if len(vector1) != len(vector2):
+        raise IndexError('Vectors must have the same numbers of coordinates')   
+    else:
+        result = []
+        for coordinatesTuple in zip(vector1, vector2):
+            result.append(coordinatesTuple[0] - coordinatesTuple[1])
+        return result
